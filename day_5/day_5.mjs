60120b1f7c7ae8859a6eefa9189de2dcccc6b2e5
Jacob Ridgwell <jacobridgwell@gmail.com>
2022-12-11 10:04:45 -0800

    day 5

--- day_5/day_5.mjs
+++ day_5/day_5.mjs
@@ -2,24 +2,40 @@ import fs from "fs";
 import { performance } from "perf_hooks";
 
 const input = fs.readFileSync("day_5/input.txt", "utf8");
+const [arrangement, procedure] = input.split("\n\n");
 const t0 = performance.now();
 
-const [arrangement, procedure] = input.split("\n\n");
 const interval = 4;
 const lineLength = arrangement.indexOf("\n");
 let stacks = [];
 
 for (let i = 0; i < (lineLength + 1) / interval; i++) {
-	stacks.push([]);
+  stacks.push([]);
 }
 
 for (let i = 0; i < arrangement.length; i++) {
-	if (arrangement.at(i) === "\n") {
-		for (let j = 0; j < (lineLength + 1) / interval; j++) {
-			const start = j * interval;
-			stacks.at(j).unshift(arrangement.slice(i - lineLength + start, i - lineLength + start + 3));
-		}
-	}
+  if (arrangement.at(i) === "\n") {
+    for (let j = 0; j < (lineLength + 1) / interval; j++) {
+      const start = j * interval + 1;
+      const char = arrangement.slice(
+        i - lineLength + start,
+        i - lineLength + start + 1
+      );
+      if (char !== " ") stacks.at(j).unshift(char);
+    }
+  }
 }
 
-console.log(stacks);
+const moves = procedure.match(/\d+/g);
+
+
+for (let i = 0; i < moves.length; i += 3) {
+  stacks[moves[i + 2] - 1].push(...stacks[moves[i + 1] - 1].splice(moves[i] * -1));
+}
+
+for (const stack of stacks) {
+  process.stdout.write(stack[stack.length - 1]);
+}
+process.stdout.write("\n");
+
+console.log(performance.now() - t0, "milliseconds");
